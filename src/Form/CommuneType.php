30a652a50cbaa64a80cd0866f780062a66e2b3c6
Anacomb <huugoo60de@gmail.com>
2020-04-29 15:17:54 +0200

    Installation bootstrap + crud Module Commune

---
+++ src/Form/CommuneType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commune;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommuneType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('code_postal')
+            ->add('region')
+            ->add('Service')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Commune::class,
+        ]);
+    }
+}
